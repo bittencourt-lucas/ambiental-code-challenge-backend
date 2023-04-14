d5bcd9abaaf99771673f1d64902f0b3edf7ba6eb
Lucas Bittencourt <lucascafieiro@hotmail.com>
2023-04-14 20:49:44 -0300

    chore: updated pylintrc to accept classes with fewer public methods and updated test to be pylint compliant

--- cron/tests/test_fetch_forecast_job.py
+++ cron/tests/test_fetch_forecast_job.py
@@ -1,9 +1,10 @@
-import pytest
-from httpx import Response
 from datetime import datetime
+from httpx import Response
+import pytest
 from app.jobs.fetch_forecast_job import FetchForecastJob
 
 CURRENT_TIMESTAMP = datetime.now()
+
 
 class MockResponse:
     def __init__(self):
@@ -14,15 +15,15 @@ class MockResponse:
         return {"mock_info": "mock"}
 
 
-@pytest.fixture
-def mock_response(monkeypatch):
-    def mock_execute(*args, **kwargs) -> tuple:
+@pytest.fixture(name="_mock_response")
+def fixture_mock_response(monkeypatch):
+    def mock_execute() -> tuple:
         return (MockResponse(), CURRENT_TIMESTAMP)
 
     monkeypatch.setattr(FetchForecastJob, "execute", mock_execute)
 
 
-def test_fetch_forecast_job(mock_response):
+def test_fetch_forecast_job(_mock_response):
     data: dict = {"mock_info": "mock"}
     status_code: int = 200
 
