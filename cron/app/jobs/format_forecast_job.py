0d6fd810b7c84d4c66629212c950567e3268c7f2
Lucas Bittencourt <lucascafieiro@hotmail.com>
2023-04-14 02:36:31 -0300

    fix: corrected a minor error when processing data, and refactored some of the code for cleanliness

--- cron/app/jobs/format_forecast_job.py
+++ cron/app/jobs/format_forecast_job.py
@@ -7,22 +7,45 @@ class FormatForecastJob:
     @staticmethod
     def execute(data: Tuple[Response, datetime]) -> dict:
         response, timestamp = data
-        hourly_entries = response.json()["hourly"]
-        valid_entries = []
-        
-        for index, entry in enumerate(hourly_entries["winddirection_180m"]):
-            if entry >= 130 and entry <= 230:
-                valid_entries.append({
-                    "data": hourly_entries["time"][index],
-                    "vel": hourly_entries["windspeed_180m"][index],
-                    "dir": hourly_entries["winddirection_180m"][index],
-                    "alerta": False,
-                })
-        
-        for index, entry in enumerate(valid_entries):
-            if entry["vel"] < 15:
-                valid_entries.pop(index)
-            if entry["vel"] > 20:
-                entry["alerta"] = True
-                
+        hourly_entries: dict = response.json()["hourly"]
+
+        valid_entries: list
+        valid_entries = process_wind_direction_entries(hourly_entries)
+        valid_entries = process_wind_speed_entries(valid_entries)
+
         return {"ultima_consulta": timestamp, "dados": valid_entries}
+
+
+def process_wind_direction_entries(entries: dict) -> list:
+    processed_entries: list = []
+    for index, entry in enumerate(entries["winddirection_180m"]):
+        if is_wind_direction_dangerous(entry):
+            processed_entries.append({
+                "data": entries["time"][index],
+                "vel": entries["windspeed_180m"][index],
+                "dir": entries["winddirection_180m"][index],
+                "alerta": False,
+            })
+    return processed_entries
+
+
+def is_wind_direction_dangerous(direction: int) -> bool:
+    return direction >= 130 and direction <= 230
+
+
+def process_wind_speed_entries(entries: list) -> list:
+    for index, entry in enumerate(entries):
+        if not is_wind_speed_dangerous(entry["vel"]):
+            entries[index] = None
+        if should_emit_alert(entry["vel"]):
+            entry["alerta"] = True
+    processed_entries = list(filter(None, entries))
+    return processed_entries
+
+
+def is_wind_speed_dangerous(speed: int) -> bool:
+    return speed >= 15
+
+
+def should_emit_alert(speed: int) -> bool:
+    return speed > 20
