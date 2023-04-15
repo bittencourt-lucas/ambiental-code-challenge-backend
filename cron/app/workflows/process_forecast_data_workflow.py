8aaf54d0466b64ec580602d41195776ad7df3248
Lucas Bittencourt <lucascafieiro@hotmail.com>
2023-04-15 20:55:47 -0300

    feat: containerized the cron project, and made the necessary changes to make it work with Docker

--- cron/app/workflows/process_forecast_data_workflow.py
+++ cron/app/workflows/process_forecast_data_workflow.py
@@ -6,6 +6,10 @@ from jobs.store_in_firestore_job import StoreInFirestoreJob
 class ProcessForecastDataWorkflow:
     @staticmethod
     def run():
-        forecast_data = FetchForecastJob.execute()
-        formatted_data = FilterForecastJob.execute(forecast_data)
-        StoreInFirestoreJob.execute(formatted_data)
+        try:
+            forecast_data = FetchForecastJob.execute()
+            formatted_data = FilterForecastJob.execute(forecast_data)
+            StoreInFirestoreJob.execute(formatted_data)
+            print("ProcessForecastDataWorkflow finished successfully")
+        except ValueError as exc:
+            raise ValueError("There was an error running the workflow") from exc
--- cron/main.py
+++ cron/main.py
@@ -1,16 +1,23 @@
 import os
 from crontab import CronTab
+from decouple import config
 
 
 if __name__ == "__main__":
-    NEXT_AT_MINUTE_5 = "5 * * * *"
+    NEXT_AT_MINUTE_5 = "* * * * *"
 
-    venv_path = os.path.dirname(
-        os.path.abspath(__file__)) + "/venv/bin/python3"
+    credentials = config("GOOGLE_APPLICATION_CREDENTIALS")
+    command_env = config("COMMAND_ENV")
+    output = config("OUTPUT_PARAMETERS")
+
     runner_file = os.path.dirname(
         os.path.abspath(__file__)) + "/app/runner.py"
 
     cron = CronTab(user=True)
-    job = cron.new(command=f"{venv_path} {runner_file}")
+    job = cron.new(
+        command=f"{command_env} {runner_file} {output}")
+    job.env["GOOGLE_APPLICATION_CREDENTIALS"] = credentials
+    job.env["COMMAND_ENV"] = command_env
+    job.env["OUTPUT_PARAMETERS"] = output
     job.setall(NEXT_AT_MINUTE_5)
     cron.write()
