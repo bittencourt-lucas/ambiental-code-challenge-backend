b74a19c1c6fa1fbc3cbdb9400fd8ad6c92b48c98
Lucas Bittencourt <lucascafieiro@hotmail.com>
2023-04-21 11:38:34 -0300

    hotfix: added same workaround to Firestore client in API

--- api/app/shared/database/firestore/client.py
+++ api/app/shared/database/firestore/client.py
@@ -1,11 +1,15 @@
+import json
 from decouple import config
 from google.cloud.firestore_v1 import Client
 from google.oauth2 import service_account
 
 CREDENTIALS_FILE = config("GOOGLE_APPLICATION_CREDENTIALS")
+with open(CREDENTIALS_FILE, "r", encoding="utf-8") as file:
+    CREDENTIALS_INFO = file.read()
+    file.close()
 
 
 def create_session() -> Client:
-    credentials = service_account.Credentials.from_service_account_file(
-        CREDENTIALS_FILE)
+    credentials = service_account.Credentials.from_service_account_info(
+        json.loads(CREDENTIALS_INFO, strict=False))
     return Client(credentials=credentials)
